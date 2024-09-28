cf9ccb832d9d105c8ccb04f64474bbebfce8a44a
Ramalhin <j.ramalho@live.com>
2024-09-28 10:00:58 -0300

    adding CORS, Deleting book classes

--- src/main/java/br/com/erudio/api/config/WebConfig.java
+++ src/main/java/br/com/erudio/api/config/WebConfig.java
@@ -1,10 +1,12 @@
 package br.com.erudio.api.config;
 
 import br.com.erudio.api.serializationconverter.YamlJackson2HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,10 +15,21 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns  ="";
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+        .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
     }
 
     @Override
--- src/main/java/br/com/erudio/api/model/controller/PersonController.java
+++ src/main/java/br/com/erudio/api/model/controller/PersonController.java
@@ -48,7 +48,7 @@ public class PersonController {
     public List<PersonVO> findAll() {
         return service.findAll();
     }
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "/{id}",
             produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
     @Operation(summary = "Finds a Person", description = "Finds a Person",
@@ -67,7 +67,7 @@ public class PersonController {
     public PersonVO findById(@PathVariable(value = "id") Long id) {
         return service.findById(id);
     }
-
+    @CrossOrigin(origins = {"http://localhost:8080", "https://erudio.com.br"})
     @PostMapping(
             consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  },
             produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
